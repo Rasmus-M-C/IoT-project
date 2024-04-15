9a7e8b931530ce2b2c8410a1d8ae62f906c1b873
AsgerLeth <202004818@post.au.dk>
2024-04-15 14:55:16 +0200

    Implemented Async in client connection.

--- Sensor/Program.cs
+++ Sensor/Program.cs
@@ -5,7 +5,7 @@ using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Formatter;
 using MQTTnet.Protocol;
-
+using MQTTService;
 
 namespace RaspberryPi
 {
@@ -14,32 +14,9 @@ namespace RaspberryPi
         static async Task Main()
         {
             string reading = "20.0";
-            var mqttFactory= new MqttFactory();
-            IMqttClient client = mqttFactory.CreateMqttClient();
-            var options = new MqttClientOptionsBuilder()
-                .WithTcpServer("192.168.50.93", 1883)
-                .WithCleanSession()
-                .Build();
-
-            await client.ConnectAsync(options, CancellationToken.None);
-            await PublishMessageAsync(client);
-            
-
-        }
-
-        private static async Task PublishMessageAsync(IMqttClient client)
-        {
-            string payload = "Hello!";
-            var message = new MqttApplicationMessageBuilder()
-                .WithTopic("home")
-                .WithPayload(payload)
-                .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
-                .Build();
-            if (client.IsConnected)
-            {
-                await client.PublishAsync(message);
-            }
-            
+            MQTTClient client = new MQTTClient();
+            await client.ConnectAsync();
+            await client.PublishMessageAsync(float.Parse(reading), "home");
         }
         
     }
