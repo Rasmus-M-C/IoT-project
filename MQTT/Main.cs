1de8bffd03d8594f82088a1e65313c2437f25ed6
AUMikkel <202007669@post.au.dk>
2024-04-17 12:14:24 +0200

    Try InfluxDB

--- MQTT/Main.cs
+++ MQTT/Main.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using InfluxDB.LoadEnvironment;
 using MQTT.Server;
+using InfluxDB.Test;
 
 namespace YourNamespace
 {
@@ -8,11 +11,13 @@ namespace YourNamespace
     {
         static async Task Main(string[] args) // Main method should be static
         {
+            var root = Directory.GetCurrentDirectory();
+            var dotenv = Path.Combine(root, ".env");
+            DotEnv.Load(dotenv);
+            
             // Create an instance of Server_Simple_Samples
-            await Server_Simple_Samples.Run_Server_With_Logging();
-            
-
-            
+            //await Server_Simple_Samples.Run_Server_With_Logging();
+            await InfluxDB_Samples.InfluxDBWrite("1", 25.0f, 1000.0f, 65.0f);
         }
     }
 }
---
+++ MQTT/sendData.cs
@@ -0,0 +1,97 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Diagnostics;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Writes;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace InfluxDB.Test
+{
+    public static class InfluxDB_Samples
+    {
+        // Temp, pressure, Humidity
+        public static async Task InfluxDBWrite(string sensorID ,float temp, float press, float humidity)
+        {
+            string influxUrl = "https://eu-central-1-1.aws.cloud2.influxdata.com";
+            string token = Environment.GetEnvironmentVariable("API_TOKEN");
+            Console.WriteLine(token);
+            string org = "WeatherDay Co";
+            const string bucket = "weatherdata";
+            
+            using (var client = new InfluxDBClient(influxUrl, token))
+            {
+                // Define data points
+                var dataPoints = new List<PointData>
+                {
+                    CreatePoint("home", sensorID,new Dictionary<string, object> {
+                        {"temp", temp },
+                        {"pressure", press},
+                        {"humidity", humidity}// Assuming it is in float
+                    }),
+                };
+                
+                foreach (var point in dataPoints)
+                {
+                    await client.GetWriteApiAsync().WritePointAsync(point: point, bucket: bucket, org: org);
+                    //await client.WritePointAsync(point: point, database: bucket);
+    
+                    Thread.Sleep(1000); // separate points by 1 second
+                }
+                /*
+                // Write the point to InfluxDB
+                var writeApi = client.GetWriteApi();
+                writeApi.WritePoints(dataPoints, bucket,org);
+                */
+            }
+
+            Console.WriteLine("Data written successfully to InfluxDB.");
+            
+        }
+        
+        static PointData CreatePoint(string measurement, string sensorId,Dictionary<string, object> fields)
+        {
+            // https://eu-central-1-1.aws.cloud2.influxdata.com/orgs/d119a21a75bd117f/new-user-setup/csharpe
+            var builder = PointData.Measurement(measurement);
+            builder.Tag("sensorId", sensorId);
+            foreach (var field in fields)
+            {
+                if (field.Value is string)
+                {
+                    builder = builder.Field(field.Key, (string)field.Value);
+                }
+                else if (field.Value is double)
+                {
+                    builder = builder.Field(field.Key, (double)field.Value);
+                }
+                else if (field.Value is long)
+                {
+                    builder = builder.Field(field.Key, (long)field.Value);
+                }
+                else if (field.Value is float)
+                {
+                    builder = builder.Field(field.Key, (float)field.Value);
+                }
+                else
+                {
+                    throw new ArgumentException($"Unsupported type for field '{field.Key}'.");
+                }
+            }
+
+            return builder.Timestamp(DateTime.UtcNow, WritePrecision.Ms);
+        }
+    }
+}
