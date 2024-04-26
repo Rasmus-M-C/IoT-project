cc15e155a9fc642b1369cdffa2e3c01afadb1eab
AUMikkel <202007669@post.au.dk>
2024-04-26 11:38:21 +0200

    Refactor

--- Sensor/Program.cs
+++ Sensor/Program.cs
@@ -11,8 +11,25 @@ namespace RaspberryPi
 {
     public class Program
     {
+        // Function to start a location of sensors
+        private static async Task SetupAndStartPublishers(MQTTClient client, string location)
+        {
+            // location where sensors are physically.
+            HumiditySensor sensor = new HumiditySensor();
+            PressureSensor psensor = new PressureSensor();
+            TempSensor tsensor = new TempSensor();
+            client.StartPublisher(sensor, 5, $"{location}/humidity");
+            client.StartPublisher(psensor, 5, $"{location}/pressure");
+            client.StartPublisher(tsensor, 5, $"{location}/temperature");
+            
+            await client.Subscribe($"{location}/humidity");
+            await client.Subscribe($"{location}/pressure");
+            await client.Subscribe($"{location}/temperature");
+        }
         static async Task Main()
         {
+            //MQTTClient client = new MQTTClient();
+            //await client.ConnectAsync();
             HumiditySensor sensor = new HumiditySensor();
             PressureSensor psensor = new PressureSensor();
             TempSensor tsensor = new TempSensor();
@@ -22,7 +39,6 @@ namespace RaspberryPi
             MQTTClient client = new MQTTClient();
             await client.ConnectAsync();
 
-
             client.StartPublisher(sensor, 5, "rasmus_room/humidity");
             client.StartPublisher(psensor, 5, "rasmus_room/pressure");
             client.StartPublisher(tsensor, 5, "rasmus_room/temperature");
@@ -30,6 +46,8 @@ namespace RaspberryPi
             await client.Subscribe("rasmus_room/humidity");
             await client.Subscribe("rasmus_room/pressure");
             await client.Subscribe("rasmus_room/temperature");
+            
+            //await SetupAndStartPublishers(client, "Edison");
             Console.ReadKey();
             Console.WriteLine("Press any key to exit");
 
